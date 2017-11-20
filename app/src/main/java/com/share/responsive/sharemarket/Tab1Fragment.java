cf5efc3b96c0cf828b2db7b5621d73c15735dd41
Binoy Subhakumar <subhakum@usc.edu>
2017-11-20 13:33:26 -0800

    Added fav buttons and fbbutons with proper alignment

--- app/src/main/java/com/share/responsive/sharemarket/Tab1Fragment.java
+++ app/src/main/java/com/share/responsive/sharemarket/Tab1Fragment.java
@@ -7,16 +7,33 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 /**
  * Created by Binoy on 11/20/2017.
  */
 
 public class Tab1Fragment extends Fragment{
+    ImageButton fb,favorties;
+    boolean isFavClicked=false;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
        View view=inflater.inflate(R.layout.tab1_fragment,container,false);
+       fb=(ImageButton)view.findViewById(R.id.fb);
+       favorties=(ImageButton)view.findViewById(R.id.favorites);
+       favorties.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               if(!isFavClicked){
+               favorties.setImageResource(R.drawable.filled);
+               isFavClicked=true;
+               }else{
+                   favorties.setImageResource(R.drawable.empty);
+                   isFavClicked=false;
+               }
+           }
+       });
         return view;
     }
 }
