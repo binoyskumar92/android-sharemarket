75ce9d4a7a90395b1eda00eb0b8c207f54045405
Binoy Subhakumar <subhakum@usc.edu>
2017-11-23 00:06:28 -0800

    added context menu for fav list

--- app/src/main/java/com/share/responsive/sharemarket/MainActivity.java
+++ app/src/main/java/com/share/responsive/sharemarket/MainActivity.java
@@ -9,6 +9,9 @@ import android.provider.BaseColumns;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -30,6 +33,7 @@ import java.net.URL;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 AutoCompleteTextView actv;
 String selectedSymbol="";
 String[] country_names;
@@ -41,9 +45,17 @@ ArrayList<FavoritesInfo> favInfoFromPreference;
     protected void onStart() {
         super.onStart();
         if(isFavListLoaded) {
-            favInfoFromPreference = UIUtils.getAllItemsfromSharedPreference();
-            favlistview.setAdapter(new CustomFavListAdapter());
+           redrawFavList();
         }
+    }
+
+    private void redrawFavList() {
+        favInfoFromPreference = UIUtils.getAllItemsfromSharedPreference();
+        favlistview.invalidateViews();
+        favlistview.invalidate();
+
+        favlistview.setAdapter(new CustomFavListAdapter());
+
     }
 
     @Override
@@ -121,13 +133,39 @@ ArrayList<FavoritesInfo> favInfoFromPreference;
         });
         //Favorite Listview
         favlistview=(ListView)findViewById(R.id.favoriteslist);
+
+
         if(!isFavListLoaded) {
             favInfoFromPreference = UIUtils.getAllItemsfromSharedPreference();
             favListAdapter = new CustomFavListAdapter();
             favlistview.setAdapter(favListAdapter);
             isFavListLoaded=true;
         }
+        registerForContextMenu(favlistview);
     }
+    //Context Menu for delete in favorite list
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,ContextMenu.ContextMenuInfo menuInfo) {
+        if (v.getId()==R.id.favoriteslist) {
+            AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)menuInfo;
+            menu.setHeaderTitle("Remove from Favorties?");
+            menu.add("No");
+            menu.add("Yes");
+        }
+    }
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)item.getMenuInfo();
+        int menuItemIndex = item.getItemId();
+        if(item.toString().equals("Yes")){
+            UIUtils.removeFavToSharedPreference(favInfoFromPreference.get((int) info.id).getSymbol());
+            Log.d(TAG, "<<onContextItemSelected: Deleted "+favInfoFromPreference.get((int) info.id).getSymbol()+">>");
+            Toast.makeText(getApplicationContext(),"Deleted "+favInfoFromPreference.get((int) info.id).getSymbol(),Toast.LENGTH_SHORT).show();
+            redrawFavList();
+        }
+        return true;
+    }
+
     private class CustomFavListAdapter extends ArrayAdapter<FavoritesInfo> {
         public CustomFavListAdapter() {
                 super(MainActivity.this, R.layout.favlist, favInfoFromPreference);
